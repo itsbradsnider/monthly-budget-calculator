ed90f5c5b660d69a0c08645bb505d76343e716a0
Brad Snider <brad.snider@gmail.com>
2017-07-20 21:52:19 -0700

    add prepared statements to expense-lines util

--- utils/update-expense_lines.utils.php
+++ utils/update-expense_lines.utils.php
@@ -8,13 +8,24 @@
 
     $table = "expense_lines";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn -> connect_error) {
+        die ("Connection error: " . $conn -> connect_error);
+    }
+
     if ($_REQUEST['do'] === "update") {
+
         $expense_id = $_REQUEST['expense_id'];
         $description = $_REQUEST['description'];
         $amount = $_REQUEST['amount'];
         $frequency = $_REQUEST['frequency'];
 
-        $sql = "UPDATE $table SET `description` = '$description', `amount` = '$amount', `frequency` = '$frequency' WHERE `$table`.`id` = $expense_id";
+        $stmt = $conn->prepare("UPDATE $table SET `description` = ?, `amount` = ?, `frequency` = ? WHERE `$table`.`id` = ?");
+
+        $stmt=>bind_param(siii, $description, $amount, $frequency, $expense_id);
+
+        //$sql = "UPDATE $table SET `description` = '$description', `amount` = '$amount', `frequency` = '$frequency' WHERE `$table`.`id` = $expense_id";
     }
 
 
@@ -28,7 +39,10 @@
         $amount = $_REQUEST['amount'];
         $frequency = $_REQUEST['frequency'];
 
-        $sql = "INSERT INTO $table (user, budget_name, budget_category, description, amount, frequency) VALUES ($user, $budget, $category, '$description', $amount, $frequency)";
+        $stmt = $conn->prepare("INSERT INTO $table (user, budget_name, budget_category, description, amount, frequency) VALUES (?, ?, ?, '?', ?, ?)")
+
+        $stmt->bind_param(isisii, $user, $budget, $category, $description, $amount, $frequency)
+        //$sql = "INSERT INTO $table (user, budget_name, budget_category, description, amount, frequency) VALUES ($user, $budget, $category, '$description', $amount, $frequency)";
 
     }
 
@@ -37,24 +51,25 @@
 
 
         $id = $_REQUEST['id'];
-        $sql = "DELETE FROM $table WHERE $table . id='$id'";
+
+        $stmt = $conn->prepare("DELETE FROM $table WHERE $table . id=?");
+
+        $stmt->bind_param(i, $id)
+        //$sql = "DELETE FROM $table WHERE $table . id='$id'";
 
 
     }
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn -> connect_error) {
-        die ("Connection error: " . $conn -> connect_error);
-    }
-
-    if($conn -> query($sql) === true) {
+    /*if($conn -> query($sql) === true) {
         $last_id = $conn -> insert_id;
         echo "updated Successfully. Affected ID is: " . $last_id;
 
     } else {
         echo "Error updating record" . mysqli_error($conn);
-    }
+    }*/
 
+    $stmt -> execute();
+
+    $stmt -> close();
     $conn -> close();
 ?>
